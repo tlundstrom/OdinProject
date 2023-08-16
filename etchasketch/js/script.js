7e49c8b0c7d4278edcce5370783bb0d1a43fa6cc
Thomas Lundstrom <thomaswlundstrom@gmail.com>
2023-08-16 16:17:53 -0700

    added drawing capability

--- etchasketch/js/script.js
+++ etchasketch/js/script.js
@@ -1,18 +1,30 @@
-let size = 16;
+let size = 200;
+
+let mouseDown = false;
+document.body.onmousedown = () => mouseDown = true;
+document.body.onmouseup= () => mouseDown = false;
+
+
 
 const gridWindow = document.getElementById('window');
 
-console.log(gridWindow);
-
 const createGrid = (size) => {
     for(let i=0; i<size*size; i++){
-        console.log('test')
         const cell = document.createElement('div');
         cell.classList.add('cell');
+        cell.addEventListener('mouseover', draw);
+        cell.addEventListener('mousedown', draw);
         gridWindow.appendChild(cell);
     }
 }
 
+const draw = (e) => {
+    if(e.type === 'mouseover' && !mouseDown) return;
+    e.target.style.background = "black";
+}
+
+
+
 window.onload = () => {
-    createGrid(16);
+    createGrid(size);
 }
