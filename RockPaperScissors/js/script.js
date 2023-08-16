9e4375587775870936c33522e6638dde85deda73
Thomas Lundstrom <thomaswlundstrom@gmail.com>
2023-08-16 16:26:12 -0700

    Changed to switch, removed unnecessary code

--- RockPaperScissors/js/script.js
+++ RockPaperScissors/js/script.js
@@ -1,66 +1,41 @@
-let computerSelection = "";
-let playerSelection = "";
-
 const getRandInt =  () => {
     return Math.floor(Math.random()* 3);
 }
 
 const getComputerChoice = (num) => {
-    if(num<1){
-        return "rock";
-    }else if(num>1){
-        return "paper";
+    switch(num){
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break;
+        case 2:
+            return "scissors";
+            break;
     }
-    return "scissors";
-}
-
-const getPlayerSelection = () => {
-    let userInput = prompt("Do you choose rock, paper, or scissors?");
-    return userInput.toLowerCase().trim();
-}
-
-const setPlayerSelection = (string) => {
-    playerSelection = string;
-}
-const setComputerSelection = () => {
-    computerSelection = getComputerChoice(getRandInt());
 }
 
 const winSituation = (computer, player) => {
-    if((computer === "rock" && player === "paper") || 
-    (computer === "paper" && player === "scissors") || 
-    (computer === "scissors" && player === "rock")){
-        return "You win, " + player + " beats " + computer + ".";
-    }else if(player === computer){
+    if(player === computer){
         return "Draw, try again."
-    }else{
-        return "You lose, " + computer + " beats " + player + ".";
+    }else if(
+        (computer === "rock" && player === "paper") || 
+        (computer === "paper" && player === "scissors") || 
+        (computer === "scissors" && player === "rock")){
+            return "You win, " + player + " beats " + computer + ".";
     }
+    return "You lose, " + computer + " beats " + player + ".";
+
 }
 
-const setResult = () =>{
-
+const setResult = (computerSelection, playerSelection) =>{
     document
         .getElementById("result")
         .innerHTML = winSituation(computerSelection, playerSelection);
-
 }
 
-const playRound = (string) => {
-    setComputerSelection();
-    setPlayerSelection(string);
-    setResult();
+const playRound = (str) => {
+    setResult(str, getComputerChoice());
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
