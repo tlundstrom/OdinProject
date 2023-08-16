92ee6418eec0214850cbf5d11266b6dbbcfed67a
Thomas Lundstrom <thomaswlundstrom@gmail.com>
2023-08-16 11:26:13 -0700

    Bare bones, next section will revisit

--- RockPaperScissors/js/script.js
+++ RockPaperScissors/js/script.js
@@ -14,17 +14,6 @@ const getComputerChoice = (num) => {
     return "scissors";
 }
 
-const winSituation = (computer, player) => {
-    if((computer === "rock" && player === "paper") || 
-    (computer === "paper" && player === "scissors") || 
-    (computer === "scissors" && player === "rock")){
-        return "You win, " + player + " beats " + computer + ".";
-    }else if(player === computer){
-        return "Draw, try again."
-    }
-    return "You lose, " + computer + " beats " + player + ".";
-}
-
 const getPlayerSelection = () => {
     let userInput = prompt("Do you choose rock, paper, or scissors?");
     return userInput.toLowerCase().trim();
@@ -32,18 +21,38 @@ const getPlayerSelection = () => {
 
 const setPlayerSelection = () => {
     playerSelection = getPlayerSelection();
+    console.log(playerSelection);
 }
 const setComputerSelection = () => {
     computerSelection = getComputerChoice(getRandInt());
+    console.log(computerSelection);
+}
+
+const winSituation = (computer, player) => {
+    if((computer === "rock" && player === "paper") || 
+    (computer === "paper" && player === "scissors") || 
+    (computer === "scissors" && player === "rock")){
+        return "You win, " + player + " beats " + computer + ".";
+    }else if(player === computer){
+        return "Draw, try again."
+    }else{
+        return "You lose, " + computer + " beats " + player + ".";
+    }
 }
 
 const playRound = () => {
     setComputerSelection();
     setPlayerSelection();
-    winSituation();
+    alert(winSituation(computerSelection, playerSelection));
 }
 
+const game = () => {
+    for(let i=0;i<4;i++){
+        playRound();
+    }
+}
 
+game();
 
 
 
