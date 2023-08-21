bfc64f5c62e41f76567afbb3e31bc585312adfbf
Thomas Lundstrom <thomaswlundstrom@gmail.com>
2023-08-21 09:36:54 -0700

    added bootstrap and an object creator

---
+++ library_app/js/script.js
@@ -0,0 +1,10 @@
+const myLibrary = [];
+const App = document.getElementById
+
+const Book = (author, title, pages, read) => {
+    return {author, title, pages, read}
+}
+
+const addBooktoLibrary = () => {
+
+}
