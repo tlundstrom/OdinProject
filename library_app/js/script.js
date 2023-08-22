60f843763fa8a2d063a5fb07e295ac64bce690f3
Thomas Lundstrom <thomaswlundstrom@gmail.com>
2023-08-22 10:29:17 -0700

    Finished cards buttons functionality

--- library_app/js/script.js
+++ library_app/js/script.js
@@ -21,16 +21,36 @@ class Library{
         this.books.push(newBook);
     }
 
+    deleteBook = (title) => {
+        let counter = 0;
+        this.getBook(title);
+        this.books.forEach((book)=>{
+            if(book.title === title){
+                this.books.splice(counter,1);
+            }
+            counter++
+        })
+        renderLibrary();
+    }
+
     getLibrary = () => {
         return this.books;
     }
 
-    getBook(title) {
+    getBook = (title) => {
         return this.books.find((book) => book.title === title)
-      }
+    }
 }
 
 const myLibrary = new Library;
+
+
+
+myLibrary.setLibrary(new Book("Misery", "Stephen King", 574, true));
+myLibrary.setLibrary(new Book("The Giver", "Lois Lowry", 774, true));
+myLibrary.setLibrary(new Book("Meditations", "Marcus Aurelius", 367, true));
+myLibrary.setLibrary(new Book("1984", "George Orwell", 328, true));
+myLibrary.setLibrary(new Book("Animal Farm", "George Orwell", 140, true));
 
 const getFormInput = ()=>{
     const title = document.getElementById('title').value;
@@ -54,27 +74,35 @@ const clearModal = () => {
     })
 }
 
+const removeBook = (e) => {
+    const title = e.target.parentNode.firstChild.innerHTML.replaceAll(
+        '"',
+        ''
+    )
+    myLibrary.deleteBook(title);
+}
+
 const toggleRead = (e) => {
     const title = e.target.parentNode.firstChild.innerHTML.replaceAll(
         '"',
         ''
-        )
+    )
     myLibrary.getBook(title).read = !myLibrary.getBook(title).read;
     renderLibrary();
-    
 }
 
 const renderLibrary = ()=>{
     const App = document.getElementById('App');
     let html = ''
     myLibrary.getLibrary().forEach(book => {
-        html += '<div className="bookCard">';
+        html += '<div class="bookCard bg-light">';
         html += '<h5>' + book.title + '</h5>';
-        html += '<p>By:' + book.author + '</p>';
-        html += '<p>Pages:' + book.pages + '</p>';
-        html += book.read===true? '<button onclick="toggleRead(event)" class="btn">Read</button>': '<button onclick="toggleRead(event)" class="btn">Unread</button>'
+        html += '<p>By: ' + book.author + '</p>';
+        html += '<p>Pages: ' + book.pages + '</p>';
+        html += book.read===true? '<button class="cardButton btn btn-sm btn-success" onclick="toggleRead(event)">Read</button>': '<button onclick="toggleRead(event)" class="cardButton btn btn-sm btn-secondary">Unread</button>'
+        html += '<button onclick="removeBook(event)" class="btn btn-sm btn-danger">Delete</button>'
         html += '</div>'
     });
     App.innerHTML = html;
-
 }
+renderLibrary();
